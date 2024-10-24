21ba22e38f02e3cab41471e1fa354b1eb42427b7
ruhulashazid <ruhulaminshazid@gmail.com>
2024-10-25 02:16:12 +0600

    commit for fix

--- src/routes/common.ts
+++ src/routes/common.ts
@@ -9,7 +9,7 @@ router.post(
   "/upload",
   uploader.single("image"),
   async function (req: any, res: any) {
-    const image = req.file as any | null;
+    const image = req.file as Express.Multer.File | null;
 
     if (!image) {
       return res.status(422).json({
